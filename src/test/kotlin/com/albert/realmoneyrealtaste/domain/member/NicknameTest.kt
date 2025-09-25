eb25ad98d21c198d36edb875cb6ed689d936d98a
ALbertIM0427 <guang7226961@naver.com>
2025-09-26 00:02:27 +0900

    test: 닉네임 유효성 검사에서 특수 문자에 대한 테스트 개선

--- src/test/kotlin/com/albert/realmoneyrealtaste/domain/member/NicknameTest.kt
+++ src/test/kotlin/com/albert/realmoneyrealtaste/domain/member/NicknameTest.kt
@@ -1,6 +1,8 @@
 package com.albert.realmoneyrealtaste.domain.member
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
@@ -48,14 +50,17 @@ class NicknameTest {
         }
     }
 
-    @Test
-    fun `test invalid nickname - special characters`() {
-        val shortNickname = "A".repeat(21)
-
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "Invalid!", "Name With Spaces", "Name@Domain", "Name#1", "Name$"
+        ]
+    )
+    fun `test invalid nickname - special characters`(invalidNickname: String) {
         assertFailsWith<IllegalArgumentException> {
-            Nickname(shortNickname)
+            Nickname(invalidNickname)
         }.let {
-            assertEquals("닉네임은 2-20자 사이여야 합니다", it.message)
+            assertEquals("닉네임은 한글, 영문, 숫자만 사용 가능합니다", it.message)
         }
     }
 }
