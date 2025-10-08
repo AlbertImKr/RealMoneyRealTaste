6160796bc74a22972c0e421a6a35aa93c99229d6
ALbertIM0427 <guang7226961@naver.com>
2025-10-08 15:46:51 +0900

    test: ActivationTokenGenerator의 테스트 케이스 추가

--- src/main/kotlin/com/albert/realmoneyrealtaste/application/member/service/UuidActivationTokenGenerator.kt
+++ src/main/kotlin/com/albert/realmoneyrealtaste/application/member/service/UuidActivationTokenGenerator.kt
@@ -3,6 +3,8 @@ package com.albert.realmoneyrealtaste.application.member.service
 import com.albert.realmoneyrealtaste.application.member.provided.ActivationTokenGenerator
 import com.albert.realmoneyrealtaste.application.member.required.ActivationTokenRepository
 import com.albert.realmoneyrealtaste.domain.member.ActivationToken
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -12,11 +14,13 @@ import java.util.UUID
 @Service
 class UuidActivationTokenGenerator(
     private val activationTokenRepository: ActivationTokenRepository,
+    @PersistenceContext private val entityManager: EntityManager,
 ) : ActivationTokenGenerator {
 
     override fun generate(memberId: Long, expirationHours: Long): ActivationToken {
         activationTokenRepository.findByMemberId(memberId)?.let {
             activationTokenRepository.delete(it)
+            entityManager.flush()
         }
 
         val createdAt = LocalDateTime.now()
