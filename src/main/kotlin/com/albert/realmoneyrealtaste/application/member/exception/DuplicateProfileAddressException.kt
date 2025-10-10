2713c9a4183098cf24de08e6f2fae162e0ad81da
ALbertIM0427 <guang7226961@naver.com>
2025-10-11 08:44:31 +0900

    docs: 예외 클래스에 대한 주석 추가

--- src/main/kotlin/com/albert/realmoneyrealtaste/application/member/exception/DuplicateProfileAddressException.kt
+++ src/main/kotlin/com/albert/realmoneyrealtaste/application/member/exception/DuplicateProfileAddressException.kt
@@ -1,3 +1,8 @@
 package com.albert.realmoneyrealtaste.application.member.exception
 
+/**
+ * 이미 사용 중인 프로필 주소에 대해 발생하는 예외
+ *
+ * @param message 예외 메시지 (기본값: "이미 사용 중인 프로필 주소입니다.")
+ */
 class DuplicateProfileAddressException(message: String = "이미 사용 중인 프로필 주소입니다.") : RuntimeException(message)
