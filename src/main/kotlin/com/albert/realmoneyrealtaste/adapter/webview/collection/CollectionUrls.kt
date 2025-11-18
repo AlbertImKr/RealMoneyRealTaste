24d6d33a82eec61b919b16faa1cd680207288e82
ALbertIM0427 <guang7226961@naver.com>
2025-11-18 17:56:16 +0900

    feat: 컬렉션 상세보기 경로 수정 및 수정 버튼 링크 변경

--- src/main/kotlin/com/albert/realmoneyrealtaste/adapter/webview/collection/CollectionUrls.kt
+++ src/main/kotlin/com/albert/realmoneyrealtaste/adapter/webview/collection/CollectionUrls.kt
@@ -4,5 +4,5 @@ object CollectionUrls {
     const val MY_LIST_FRAGMENT = "/collections/my-list/fragment"
     const val WRITE_FRAGMENT = "/collections/write/fragment"
     const val COLLECTION_POSTS_FRAGMENT = "/collections/{collectionId}/posts/fragment"
-    const val DETAIL_FRAGMENT = "/collections/{collectionId}/fragment"
+    const val DETAIL_FRAGMENT = "/collections/{collectionId}/edit/fragment"
 }
