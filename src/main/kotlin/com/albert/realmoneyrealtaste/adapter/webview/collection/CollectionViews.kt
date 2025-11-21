e515534fadccf9083e9031788f70129860073c14
ALbertIM0427 <guang7226961@naver.com>
2025-11-22 07:50:48 +0900

    refactor: 컬렉션 프래그먼트를 개별 파일로 분리 및 모듈화
    Frontend:
    - collection/fragment.html을 5개 개별 프래그먼트 파일로 분리
    - collections-content.html: 컬렉션 리스트 프래그먼트
    - write-collection.html: 컬렉션 생성 폼 프래그먼트
    - manage-collection-posts.html: 컬렉션 게시글 관리 프래그먼트
    - edit-collection.html: 컬렉션 수정 폼 프래그먼트
    - collection-detail.html: 컬렉션 상세보기 프래그먼트
    
    Backend:
    - CollectionViews.kt에서 프래그먼트 경로를 collection/fragments/로 업데이트
    
    Features:
    - 프래그먼트 모듈화로 재사용성 향상
    - 독립적인 파일 관리로 유지보수성 개선
    - 코드 가독성 및 관리 용이성 증대
    - 템플릿 구조의 명확한 분리

--- src/main/kotlin/com/albert/realmoneyrealtaste/adapter/webview/collection/CollectionViews.kt
+++ src/main/kotlin/com/albert/realmoneyrealtaste/adapter/webview/collection/CollectionViews.kt
@@ -1,9 +1,9 @@
 package com.albert.realmoneyrealtaste.adapter.webview.collection
 
 object CollectionViews {
-    const val MY_LIST = "collection/fragment :: collections-content"
-    const val WRITE_FRAGMENT = "collection/fragment :: write-collection"
-    const val COLLECTION_POSTS_FRAGMENT = "collection/fragment :: manage-collection-posts"
-    const val DETAIL_EDIT_FRAGMENT = "collection/fragment :: edit-collection"
-    const val DETAIL_FRAGMENT = "collection/fragment :: collection-detail"
+    const val MY_LIST = "collection/fragments/collections-content :: collections-content"
+    const val WRITE_FRAGMENT = "collection/fragments/write-collection :: write-collection"
+    const val COLLECTION_POSTS_FRAGMENT = "collection/fragments/manage-collection-posts :: manage-collection-posts"
+    const val DETAIL_EDIT_FRAGMENT = "collection/fragments/edit-collection :: edit-collection"
+    const val DETAIL_FRAGMENT = "collection/fragments/collection-detail :: collection-detail"
 }
