6b58cfcff26a4f1f9caa3ec4993d3ff04122048c
ALbertIM0427 <guang7226961@naver.com>
2025-11-26 09:17:28 +0900

    fix(email): Spring Mail 발신자 이메일 주소 설정 누락 문제 해결
    - EmailSenderImpl에 `@Value("${spring.mail.username}") 어노테이션 추가`
    - helper.setFrom(fromEmail) 호출로 발신자 이메일 주소 설정
    - 테스트 코드에서 생성자 파라미터 추가에 따른 수정
    - "can't determine local email address" 오류 해결

--- src/main/kotlin/com/albert/realmoneyrealtaste/adapter/integration/email/EmailSenderImpl.kt
+++ src/main/kotlin/com/albert/realmoneyrealtaste/adapter/integration/email/EmailSenderImpl.kt
@@ -2,6 +2,7 @@ package com.albert.realmoneyrealtaste.adapter.integration.email
 
 import com.albert.realmoneyrealtaste.application.member.required.EmailSender
 import com.albert.realmoneyrealtaste.domain.member.value.Email
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Profile
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service
 @Profile("!test")
 class EmailSenderImpl(
     private val mailSender: JavaMailSender,
+    @Value("\${spring.mail.username}") private val fromEmail: String,
 ) : EmailSender {
 
     override fun send(
@@ -22,6 +24,7 @@ class EmailSenderImpl(
         val message = mailSender.createMimeMessage()
         val helper = MimeMessageHelper(message, true, "UTF-8")
 
+        helper.setFrom(fromEmail)
         helper.setTo(to.address)
         helper.setSubject(subject)
         helper.setText(content, isHtml)
